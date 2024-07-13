ab2e97ffc623acf49ccfdeb4516167246f2eb6d6
cardim <pedrogscardim@gmail.com>
2024-07-13 14:20:08 -0300

    commit Tesla

---
+++ Tesla/scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "40%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
